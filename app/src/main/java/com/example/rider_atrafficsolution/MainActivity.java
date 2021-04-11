9dd674688835abd5223df653035476fc9823bf5e
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-04-11 13:58:05 +0600

    ./idea removed

--- app/src/main/java/com/example/rider_atrafficsolution/MainActivity.java
+++ app/src/main/java/com/example/rider_atrafficsolution/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rider_atrafficsolution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
