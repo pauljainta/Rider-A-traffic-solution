cd97bba6cc1da4049ee5897356ae1a5ba94073e5
Towsif20 <aaatowsif16@gmail.com>
2021-06-05 00:19:48 +0600

    bus selection ui design start

---
+++ app/src/main/java/com/example/rider_atrafficsolution/BusSeatSelection.java
@@ -0,0 +1,16 @@
+package com.example.rider_atrafficsolution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BusSeatSelection extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_seat_selection);
+    }
+}
