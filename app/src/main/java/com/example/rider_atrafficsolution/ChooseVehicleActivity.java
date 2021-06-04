191d896c37dd757c31b778211d36f33b465dad64
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-06-05 00:26:58 +0600

    Vehicle selection UI design almost done

--- app/src/main/java/com/example/rider_atrafficsolution/ChooseVehicleActivity.java
+++ app/src/main/java/com/example/rider_atrafficsolution/ChooseVehicleActivity.java
@@ -2,15 +2,45 @@ package com.example.rider_atrafficsolution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class ChooseVehicleActivity extends AppCompatActivity
+public class ChooseVehicleActivity extends AppCompatActivity implements View.OnClickListener
 {
+    ImageButton busChooseButton,bikeChooseButton,carChooseButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_vehicle);
+        busChooseButton=findViewById(R.id.busChooseButton);
+        bikeChooseButton=findViewById(R.id.bikeChooseButton);
+        carChooseButton=findViewById(R.id.carChooseButton);
+        busChooseButton.setOnClickListener(this);
+        carChooseButton.setOnClickListener(this);
+        bikeChooseButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.busChooseButton:
+
+                break;
+
+            case R.id.bikeChooseButton:
+                break;
+
+            case R.id.carChooseButton:
+                break;
+        }
     }
 }
