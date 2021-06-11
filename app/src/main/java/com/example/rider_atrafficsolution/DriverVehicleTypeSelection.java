c22a676a6c36e95890495284ad02babbf3bd4bfd
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-06-11 23:22:38 +0600

    DriverVehicleTypeSelection  class added

---
+++ app/src/main/java/com/example/rider_atrafficsolution/DriverVehicleTypeSelection.java
@@ -0,0 +1,14 @@
+package com.example.rider_atrafficsolution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverVehicleTypeSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_vehicle_type_selection);
+    }
+}
