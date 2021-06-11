95b22eead77a4413e9ea742de3e14c154f719888
Towsif20 <aaatowsif16@gmail.com>
2021-06-11 23:13:03 +0600

    bus activities completed for now

--- app/src/main/java/com/example/rider_atrafficsolution/BusJourneyCompleteActivity.java
+++ app/src/main/java/com/example/rider_atrafficsolution/BusJourneyCompleteActivity.java
@@ -2,15 +2,34 @@ package com.example.rider_atrafficsolution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class BusJourneyCompleteActivity extends AppCompatActivity
 {
+    TextView busFareView;
+    Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_journey_complete);
+
+        busFareView = findViewById(R.id.busFareTextView);
+        continueButton = findViewById(R.id.continueButton);
+
+        double fare = getIntent().getDoubleExtra("fare", 1);
+
+        busFareView.setText("Your Fare was \n" + fare + "Taka");
+    }
+
+    public void gotoHome(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), ChooseVehicleActivity.class);
+        startActivity(intent);
     }
 }
