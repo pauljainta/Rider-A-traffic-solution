74039de8e3a575e469c239f8f914670b4750a426
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-06-18 19:55:35 +0600

    nav bar almost complete

--- app/src/main/java/com/example/rider_atrafficsolution/ChooseVehicleActivity.java
+++ app/src/main/java/com/example/rider_atrafficsolution/ChooseVehicleActivity.java
@@ -1,17 +1,26 @@
 package com.example.rider_atrafficsolution;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ChooseVehicleActivity extends AppCompatActivity implements View.OnClickListener
 {
     ImageButton busChooseButton,bikeChooseButton,carChooseButton;
+    NavigationView naview;
+    TextView username,rating;
+
 
 
     @Override
@@ -22,9 +31,34 @@ public class ChooseVehicleActivity extends AppCompatActivity implements View.OnC
         busChooseButton=findViewById(R.id.busChooseButton);
         bikeChooseButton=findViewById(R.id.bikeChooseButton);
         carChooseButton=findViewById(R.id.carChooseButton);
+        naview = findViewById(R.id.nav_view);
+        username=findViewById(R.id.nav_header_title);
+        rating=findViewById(R.id.nav_header_subtitle);
         busChooseButton.setOnClickListener(this);
         carChooseButton.setOnClickListener(this);
         bikeChooseButton.setOnClickListener(this);
+
+
+
+        naview.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.nav_history:
+                        break;
+
+                    case R.id.nav_help:
+                        break;
+
+                    case R.id.nav_logout:
+                        break;
+
+                }
+                return true;
+            }
+        });
 
     }
 
