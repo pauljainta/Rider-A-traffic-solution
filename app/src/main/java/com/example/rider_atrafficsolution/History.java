83ddf5454b746300802943a0a7daffd2494f9996
pauljainta <58131098+pauljainta@users.noreply.github.com>
2021-06-27 18:18:23 +0600

    History class added

---
+++ app/src/main/java/com/example/rider_atrafficsolution/History.java
@@ -0,0 +1,77 @@
+package com.example.rider_atrafficsolution;
+
+public class History {
+   public String source;
+   public String dest;
+   public String driverName;
+   public String passengerName;
+   public String date;
+   public String travelTime;
+   public String userRating;
+   public String driverRating;
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDest() {
+        return dest;
+    }
+
+    public void setDest(String dest) {
+        this.dest = dest;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTravelTime() {
+        return travelTime;
+    }
+
+    public void setTravelTime(String travelTime) {
+        this.travelTime = travelTime;
+    }
+
+
+    public String getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(String userRating) {
+        this.userRating = userRating;
+    }
+
+    public String getDriverRating() {
+        return driverRating;
+    }
+
+    public void setDriverRating(String driverRating) {
+        this.driverRating = driverRating;
+    }
+}
